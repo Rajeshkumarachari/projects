71e71e57f3da9be06d39955c44fd4186377e01b6
Rajeshkumarachari <131753283+Rajeshkumarachari@users.noreply.github.com>
2024-02-29 08:50:46 +0530

    todo created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Calender from "./project/calender/Calender";
 import { Link } from "react-router-dom";
 import Quiz from "./project/quiz/Quiz";
 import { SiFramework } from "react-icons/si";
-import Accordion from "./project/accordion/Accordion";
+import Todos from "./project/todo/Todos";
 
 function App() {
   return (
@@ -13,19 +12,15 @@ function App() {
         <nav className="navbar">
           <SiFramework className="navLogo" />
           <Link className="navlink" to="/">
-            Calender
+            Todo App
           </Link>
           <Link className="navlink" to="/quiz">
             Quiz App
           </Link>
-          <Link className="navlink" to="/accordian">
-            Accordion
-          </Link>
         </nav>
         <Routes>
-          <Route path="/" element={<Calender />} />
+          <Route path="/" element={<Todos />} />
           <Route path="/quiz" element={<Quiz />} />
-          <Route path="/accordian" element={<Accordion />} />
         </Routes>
       </BrowserRouter>
     </>
